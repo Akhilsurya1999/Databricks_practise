a5d55e1dedf92e7bd715f473a74960c89b124bd2
Akhilsurya1999 <fake2ravulaakhilsurya1999@gmail.com>
2026-05-20 19:23:30 +0000

    2nd commit

--- practise2/Greeting Message.py
+++ practise2/Greeting Message.py
@@ -3,7 +3,7 @@
 # [tool.databricks.environment]
 # environment_version = "5"
 # ///
-
 print("Hello Akhil, keep up th motivation to learn you are going to sucessed")
 print("commit 4")
 print("latest comment in commit4")
+print("2nd commit from branch4")
